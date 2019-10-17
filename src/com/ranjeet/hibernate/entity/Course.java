ae46c9f058648ec050ffc5e3d76ffd40fe59324b
imranjeetsingh <ranjee970@gmail.com>
2019-10-17 17:50:30 +0530

    review class

--- src/com/ranjeet/hibernate/entity/Course.java
+++ src/com/ranjeet/hibernate/entity/Course.java
@@ -1,13 +1,18 @@
 package com.ranjeet.hibernate.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +31,10 @@ public class Course {
 							CascadeType.PERSIST,CascadeType.REFRESH})
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
 	
 	public Course() {
 		// TODO Auto-generated constructor stub
@@ -59,6 +68,22 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+	
+	public void addReview(Review theReview) {
+		
+		if(theReview==null) {
+			reviews = new ArrayList<>(); 
+		}
+		reviews.add(theReview);
+	}
+	
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + "]";
